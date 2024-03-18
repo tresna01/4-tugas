59edb2bbc635904da4e3aba6b66f223aa8c2bf4c
tresna01 <148548388+tresna01@users.noreply.github.com>
2024-03-18 12:23:59 +0700

    Add files via upload

--- bilanganGanjil&Genap.py
+++ bilanganGanjil&Genap.py
@@ -1,10 +1,12 @@
-a=0
+a= int(input("Masukkan angka: "))
 
-if a > 0 and  a %2 == 1:
+if a > 0 and  a %2 == 0:
+    print(f"{a} bilangan Positif & Genap")
+elif a > 0 and a %2 == 1:
     print(f"{a} bilangan Positif & Ganjil")
-elif a > 0 and a %2 == 0:
-    print(f"{a} bilangan Negatif & Genap")
 elif a < 0 and a %2 == 0:
-    print(f"{a} bilangan Negatif dan Ganjil")
+    print(f"{a} bilangan Negatif & Genap")
+elif a < 0 and a %1 == 0:
+    print(f"{a} bilangan Negatif & Ganjil")
 else:
-    (f"{a} bilangan Netral")
+    print (f"{a} bilangan Netral")
